a5bd4b2f590a78ef4202674480ba2e1a077935fc
Alexander Orlovsky <javaprofi@gmx.de>
2021-01-17 16:09:22 +0100

    testng testcase

--- AssertJWorkshop/src/test/java/tests/StringTest.java
+++ AssertJWorkshop/src/test/java/tests/StringTest.java
@@ -1,14 +1,31 @@
 package tests;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+//For this test you need TestNG runner
+public class StringTest {
 
-import org.junit.jupiter.api.Test;
-
-class StringTest {
-
-	@Test
-	void stringTest(String input) {
-
+	@Test(dataProvider = "simpledataprovidertest")
+	void stringTest(
+			String input) {
+		assertSoftly(
+				softAssertions -> {
+					softAssertions
+							.assertThat(input)
+							.hasSize(8)
+							.startsWith("se")
+							.doesNotContain("api")
+							.doesNotContainAnyWhitespaces()
+							.containsOnlyOnce("i");
+				});
 	}
 
+	@DataProvider(name = "simpledataprovidertest")
+	public Object[] getData() {
+		return new String[]{
+				"selenium",
+				"selenide",
+		};
+	}
 }
